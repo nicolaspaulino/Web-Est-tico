172d22f5f6e9d26bd3ca870369fed1d0241086e5
nicolaspaulino <159495947+nicolaspaulino@users.noreply.github.com>
2024-06-11 20:47:09 +0000

    vers2

--- Peso/peso.js
+++ Peso/peso.js
@@ -3,28 +3,34 @@ document.getElementById("avoid").addEventListener("click", function(event){
   });
 function Calcular (){
 
-    let Massa = document.getElementsById("Massa").value;
+    let Massa = document.getElementById("Massa").value;
     let Altura = document.getElementById("Altura").value;
     let IMC = Massa /(Altura * Altura );
-    if(IMC <= 16.9) {
+    if(Massa == 0){
+        document.getElementById("Resultado").innerHTML = "É necessario o preenchimento da massa"
+    }
+    if(Altura == 0){
+        document.getElementById("Resultado").innerHTML = "É necessario o preenchimento da altura"
+    }
+        else if(IMC <= 16.9) {
         document.getElementById("Resultado").innerHTML = "Muito abaixo do peso";
     }
-    if(IMC <= 18.4 || IMC >= 17) {
+            else if(IMC <= 18.4 || IMC >= 17) {
         document.getElementById("Resultado").innerHTML = "Abaixo do peso";
     }
-    if(IMC <=24.9 || IMC >=18.5) {
+            else if(IMC <=24.9 || IMC >=18.5) {
         document.getElementById("Resultado").innerHTML = "Peso normal";
     }
-    if(IMC <= 29.9 || IMC >= 25 ) {
+                else if(IMC <= 29.9 || IMC >= 25 ) {
         document.getElementById("Resultado").innerHTML = "Muito abaixo do peso";
     }
-    if(IMC <= 34.9 || IMC >= 30) {
+                    else if(IMC <= 34.9 || IMC >= 30) {
         document.getElementById("Resultado").innerHTML = "Obesidade grau I";
     }
-    if(IMC <= 40 || IMC >= 35) {
+                        else if(IMC <= 40 || IMC >= 35) {
         document.getElementById("Resultado").innerHTML = "Obesidade grau II";
     }
-    if(IMC > 40.0) {
+                             else if(IMC > 40.0) {
         document.getElementById("Resultado").innerHTML = "Obesidade grau III";
     }
 }
